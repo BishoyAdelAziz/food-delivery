f96dd65fc237e882b2e76aaf98c0a3364c942fc8
Bishoy Adel <bishoy.adel5555@gmail.com>
2025-01-03 05:52:34 +0200

    Improved Home Component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./Home.css";
 import Header from "../../Components/Header/Header";
 import Exploremenu from "../../Components/ExploreMenu/Exploremenu";
 import Fooddisplay from "../../Components/FoodDisplay/Fooddisplay";
 import Appdownload from "../../Components/Appdownload/Appdownload";
+import { Outlet } from "react-router-dom";
 function Home() {
   const [category, setCategory] = useState("All");
   return (
     <div>
+      <Outlet />
       <Header />
       <Exploremenu category={category} setCategory={setCategory} />
       <Fooddisplay category={category} />
