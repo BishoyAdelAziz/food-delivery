a5c6dddba4effa5fbddd2089d23a64b31fc98672
Bishoy Adel <bishoy.adel5555@gmail.com>
2025-01-03 06:07:21 +0200

    Improved Home Component Performance

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,12 +4,10 @@ import Header from "../../Components/Header/Header";
 import Exploremenu from "../../Components/ExploreMenu/Exploremenu";
 import Fooddisplay from "../../Components/FoodDisplay/Fooddisplay";
 import Appdownload from "../../Components/Appdownload/Appdownload";
-import { Outlet } from "react-router-dom";
 function Home() {
   const [category, setCategory] = useState("All");
   return (
     <div>
-      <Outlet />
       <Header />
       <Exploremenu category={category} setCategory={setCategory} />
       <Fooddisplay category={category} />
